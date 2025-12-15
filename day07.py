74e4364672b6c7265cf796f99086df0c101155f6
wjpark11 <wjpark11@gmail.com>
2025-12-15 17:46:17 +0900

    finish day 7

--- day07.py
+++ day07.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 with open("day07_input.txt", "rt") as f:
     inputs = f.readlines()
     inputs = [line.strip() for line in inputs]
@@ -20,5 +22,24 @@ def part1():
     return splitted
 
 
+# part 2
+def part2():
+    timelines = {inputs[0].index("S"): 1}
+    for fold in inputs[1:]:
+        splitters_in_current_fold = [
+            index for index, char in enumerate(fold) if char == "^"
+        ]
+        new_timelines = defaultdict(int)
+        for timeline in timelines.keys():
+            if timeline in splitters_in_current_fold:
+                new_timelines[timeline - 1] += timelines[timeline]
+                new_timelines[timeline + 1] += timelines[timeline]
+            else:
+                new_timelines[timeline] += timelines[timeline]
+        timelines = new_timelines
+    return sum(timelines.values())
+
+
 if __name__ == "__main__":
     print(part1())
+    print(part2())
