c2554db2b4e8ae3384b470e291cacc3c997edc6f
wjpark11 <wjpark11@gmail.com>
2025-12-16 15:47:03 +0900

    finish day 8

--- day08.py
+++ day08.py
@@ -4,7 +4,9 @@ with open("day08_input.txt", "rt") as f:
     inputs = f.readlines()
     inputs = [line.strip() for line in inputs]
 
-junction_boxes = [list(map(int, line.split(","))) for line in inputs]
+junction_boxes = {
+    idx: list(map(int, line.split(","))) for idx, line in enumerate(inputs)
+}
 
 
 def distance(box1, box2):
@@ -13,18 +15,82 @@ def distance(box1, box2):
     )
 
 
-def get_distance_matrix(boxes):
-    n = len(boxes)
-    dist_matrix = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if i > j:
-                dist_matrix[i][j] = distance(boxes[i], boxes[j])
-            elif i < j:
-                dist_matrix[i][j] = dist_matrix[j][i]
-            else:
-                dist_matrix[i][j] = 0
-    return dist_matrix
+# part 1
+def get_shortest_1000_pairs(boxes):
+    distances = dict()
+    for i in boxes.keys():
+        for j in boxes.keys():
+            if i < j:
+                distances[(i, j)] = distance(boxes[i], boxes[j])
+    shortest_pairs = dict(
+        sorted(distances.items(), key=lambda item: item[1])[:1000]
+    ).keys()
+    return list(shortest_pairs)
 
 
-print(get_distance_matrix(junction_boxes))
+def find_circuit(pairs, box_idx):
+    circuit = {box_idx}
+    while True:
+        added = False
+        for pair in pairs:
+            if circuit & set(pair):
+                circuit |= set(pair)
+                pairs.remove(pair)
+                added = True
+        if not added:
+            break
+    return circuit
+
+
+def part1():
+    circuit_list = []
+    connecting_pairs = get_shortest_1000_pairs(junction_boxes)
+    indexes = {pair[0] for pair in connecting_pairs} | {
+        pair[1] for pair in connecting_pairs
+    }
+    while indexes:
+        box_idx = indexes.pop()
+        circuit = find_circuit(connecting_pairs, box_idx)
+        circuit_list.append(circuit)
+        indexes -= circuit
+    circuit_sizes = sorted([len(circuit) for circuit in circuit_list], reverse=True)
+    return circuit_sizes[0] * circuit_sizes[1] * circuit_sizes[2]
+
+
+# part 2
+def get_sorted_pairs(boxes):
+    distances = dict()
+    for i in boxes.keys():
+        for j in boxes.keys():
+            if i < j:
+                distances[(i, j)] = distance(boxes[i], boxes[j])
+    sorted_pairs = dict(sorted(distances.items(), key=lambda item: item[1])).keys()
+    return list(sorted_pairs)
+
+
+box_indexes = set(junction_boxes.keys())
+
+
+def part2(all_indexes):
+    sorted_pairs = get_sorted_pairs(junction_boxes)
+    first_pair = sorted_pairs[0]
+    lower_bound = len(junction_boxes) - 1
+    upper_bound = 100000
+    half_way = (lower_bound + upper_bound) // 2
+    while True:
+        circuit = find_circuit(sorted_pairs[:half_way], first_pair[0])
+        if circuit == all_indexes:
+            upper_bound = half_way
+        else:
+            lower_bound = half_way
+        if lower_bound + 1 == upper_bound:
+            break
+        half_way = (lower_bound + upper_bound) // 2
+        print(f"Trying with {half_way} pairs...")
+    final_pair = sorted_pairs[upper_bound - 1]
+    return junction_boxes[final_pair[0]][0] * junction_boxes[final_pair[1]][0]
+
+
+if __name__ == "__main__":
+    print(part1())
+    print(part2(box_indexes.copy()))
